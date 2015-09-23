108959befb7f3f0502431691f3a2f05799f71b88
grupoiris <digital@grupoiris.co>
2015-09-23 08:33:57 -0500

    actualziacion

--- general.js
+++ general.js
@@ -1,4 +1,6 @@
 function icon_slide(action){
+	var pathname = window.location.pathname; // Returns path only
+
 	closegallery();
 	closerender();
 	closemapa();
@@ -6,15 +8,17 @@ function icon_slide(action){
 	closevideo();
 	$('.icon_slide img').attr({	src: $('.icon_slide img').attr('data-other-src')         , 'data-other-src': $('.icon_slide img').attr('src') 	    })
 	$('.icon_slide').attr({      onclick: $('.icon_slide ').attr('data-other-action')     , 'data-other-action': $('.icon_slide ').attr('onclick')    })
-	if(action =="open"){
-		$( ".sub_menu_left" ).animate({	    left: "130"	  }, 1000, function() {});
-		$( ".menu_bottom" 	).animate({	    bottom: "0"	  }, 1000, function() {});
-	}else{
-		$( ".sub_menu_left" ).animate({	    left: "0"	  }, 1000, function() {});
-		$( ".menu_bottom" 	).animate({	    bottom: "-100"	  }, 1000, function() {});
-		if($( ".content_slide" ).is(":visible")){
-			$('.sub_menu_left_item').removeClass("sub_menu_left_active");
-			$( ".content_slide" ).animate({	     opacity: 0,  }, 1000, function() {	$( ".content_slide" ).hide(); });
+	if(pathname != "/nio/monteria.html" && pathname != "nio/nio.html"){
+		if(action =="open"){
+			$( ".sub_menu_left" ).animate({	    left: "130"	  }, 1000, function() {});
+			$( ".menu_bottom" 	).animate({	    bottom: "0"	  }, 1000, function() {});
+		}else{
+			$( ".sub_menu_left" ).animate({	    left: "0"	  }, 1000, function() {});
+			$( ".menu_bottom" 	).animate({	    bottom: "-100"	  }, 1000, function() {});
+			if($( ".content_slide" ).is(":visible")){
+				$('.sub_menu_left_item').removeClass("sub_menu_left_active");
+				$( ".content_slide" ).animate({	     opacity: 0,  }, 1000, function() {	$( ".content_slide" ).hide(); });
+			}
 		}
 	}
 }
